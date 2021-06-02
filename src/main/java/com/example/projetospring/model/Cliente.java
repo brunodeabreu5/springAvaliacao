58a3594266cf943bb7fca6d04745b2d147076540
Bruno <brunodornelesdeabreu@hotmail.com>
2021-06-01 22:45:17 -0300

    acesso banco de dados

--- src/main/java/com/example/projetospring/model/Cliente.java
+++ src/main/java/com/example/projetospring/model/Cliente.java
@@ -1,4 +1,82 @@
 package com.example.projetospring.model;
 
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "cliente")
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idCliente;
+    private String nome;
+    private String endereco;
+    private String telefone;
+
+    @OneToMany(mappedBy = "cliente")
+    private List<Veiculo> veiculo = new ArrayList<>();
+
+    public Cliente (){}
+
+    public Cliente (int idCliente, String nome, String endereco, String telefone, List<Veiculo> veiculo) {
+        this.idCliente = idCliente;
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.veiculo = veiculo;
+    }
+
+    public int getIdCliente () {
+        return idCliente;
+    }
+
+    public void setIdCliente (int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNome () {
+        return nome;
+    }
+
+    public void setNome (String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco () {
+        return endereco;
+    }
+
+    public void setEndereco (String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone () {
+        return telefone;
+    }
+
+    public void setTelefone (String telefone) {
+        this.telefone = telefone;
+    }
+
+    public List<Veiculo> getVeiculo () {
+        return veiculo;
+    }
+
+    public void setVeiculo (List<Veiculo> veiculo) {
+        this.veiculo = veiculo;
+    }
+
+    @Override
+    public String toString () {
+        return "Cliente{" +
+                "idCliente=" + idCliente +
+                ", nome='" + nome + '\'' +
+                ", endereco='" + endereco + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", veiculo=" + veiculo +
+                '}';
+    }
 }
---
+++ src/main/java/com/example/projetospring/model/Veiculo.java
@@ -0,0 +1,80 @@
+package com.example.projetospring.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name ="veiculo")
+public class Veiculo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idVeiculos;
+    private String marca;
+    private String placaDoVeiculo;
+    private int ano;
+
+    @ManyToOne
+    @JoinColumn(name ="veiculo")
+    private Cliente cliente;
+
+    public Veiculo (){}
+
+    public Veiculo (int idVeiculos, String marca, String placaDoVeiculo, int ano, Cliente cliente) {
+        this.idVeiculos = idVeiculos;
+        this.marca = marca;
+        this.placaDoVeiculo = placaDoVeiculo;
+        this.ano = ano;
+        this.cliente = cliente;
+    }
+
+    public int getIdVeiculos () {
+        return idVeiculos;
+    }
+
+    public void setIdVeiculos (int idVeiculos) {
+        this.idVeiculos = idVeiculos;
+    }
+
+    public String getMarca () {
+        return marca;
+    }
+
+    public void setMarca (String marca) {
+        this.marca = marca;
+    }
+
+    public String getPlacaDoVeiculo () {
+        return placaDoVeiculo;
+    }
+
+    public void setPlacaDoVeiculo (String placaDoVeiculo) {
+        this.placaDoVeiculo = placaDoVeiculo;
+    }
+
+    public int getAno () {
+        return ano;
+    }
+
+    public void setAno (int ano) {
+        this.ano = ano;
+    }
+
+    public Cliente getCliente () {
+        return cliente;
+    }
+
+    public void setCliente (Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    @Override
+    public String toString () {
+        return "Veiculo{" +
+                "idVeiculos=" + idVeiculos +
+                ", marca='" + marca + '\'' +
+                ", placaDoVeiculo='" + placaDoVeiculo + '\'' +
+                ", ano=" + ano +
+                ", cliente=" + cliente +
+                '}';
+    }
+}
